e67e84e48af6797293d0a620467d09ea84a3af2b
Bobby Smith <bobby.smith@gmail.com>
2017-07-19 16:27:30 -0400

    [Pulse] Committing a Feature for "ServeCoffee"

--- src/test/java/com/qasymphony/demo1/ServeCoffee.java
+++ src/test/java/com/qasymphony/demo1/ServeCoffee.java
@@ -10,17 +10,19 @@ import cucumber.api.java.en.When;
 import cucumber.api.PendingException;
 
 public class ServeCoffee {
-	@Given("^there are <count> coffees left in the machine$")
-	public void thereAreCountCoffeesLeftInTheMachine(String count) throws Throwable {
-  	throw new PendingException();
+	
+	@Given("^there are (\\d+) coffees left in the machine$")
+	public void there_are_coffees_left_in_the_machine(int arg1) throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+	     throw new PendingException();
 	}
 
-	@And("^I have deposited <dollars>$")
+	@Given("^I have deposited (\\d+)\\$$")
 	public void iHaveDepositedDollars(String dollars) throws Throwable {
   	     throw new PendingException();
 	}
 
-	@And("^I have previously had <dollars> in the bank$")
+	@Given("^I previously had (\\d+)\\$ in the bank$")
 	public void iHavePreviouslyHadDollarsInTheBank(String dollars)  {
   	     throw new PendingException();
 	}
@@ -35,22 +37,5 @@ public class ServeCoffee {
   	     throw new PendingException();
 	}
 	
-	@Given("^there are (\\d+) coffees left in the machine$")
-	public void there_are_coffees_left_in_the_machine(int arg1) throws Throwable {
-	     // Write code here that turns the phrase above into concrete actions
-	     throw new PendingException();
-	}
-
-	@Given("^I have deposited (\\d+)\\$$")
-	public void i_have_deposited_$(int arg1) throws Throwable {
-	     // Write code here that turns the phrase above into concrete actions
-	     throw new PendingException();
-	}
-
-	@Given("^I previously had (\\d+)\\$ in the bank$")
-	public void i_previously_had_$_in_the_bank(int arg1) throws Throwable {
-	     // Write code here that turns the phrase above into concrete actions
-	     throw new PendingException();
-	}
 }
 
