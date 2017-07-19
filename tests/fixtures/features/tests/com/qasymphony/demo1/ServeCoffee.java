c4cb911aceaeee5a4359aab8aa96c49600dc8025
Bobby Smith <bobby.smith@gmail.com>
2017-07-19 12:03:16 -0400

    [Pulse] Test case results for  "ServeCoffee"

---
+++ tests/fixtures/features/tests/com/qasymphony/demo1/ServeCoffee.java
@@ -0,0 +1,33 @@
+package com.pulsedemo.steps;
+
+import org.junit.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.apiu.java.en.And;
+import cucumber.api.java.en.When;
+
+@Given("^there are <count> coffees left in the machine$")
+public void thereAreCountCoffeesLeftInTheMachine(String count) throws Throwable {
+  throw new PendingException();
+}
+
+@And("^I have deposited <dollars>$")
+public void iHaveDepositedDollars(String dollars) throws Throwable {
+  throw new PendingException();
+}
+
+@And("^I have previously had <dollars> in the bank$")
+public void iHavePreviouslyHadDollarsInTheBank(String dollars)  {
+  throw new PendingException();
+}
+
+@When("^I press the coffee button$")
+public void iPressTheCoffeeButton {
+  throw new PendingException();
+}
+
+@Then("^I should be served a coffee$")
+public void iShouldBeServedCoffee {
+  throw new PendingException();
+}
