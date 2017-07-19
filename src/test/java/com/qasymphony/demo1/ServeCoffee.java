3a3442c9b16f295cd66d1269e4db9f1383cf5821
Bobby Smith <bobby.smith@gmail.com>
2017-07-19 15:11:47 -0400

    Update ServeCoffee.java

--- src/test/java/com/qasymphony/demo1/ServeCoffee.java
+++ src/test/java/com/qasymphony/demo1/ServeCoffee.java
@@ -17,21 +17,39 @@ public class ServeCoffee {
 
 	@And("^I have deposited <dollars>$")
 	public void iHaveDepositedDollars(String dollars) throws Throwable {
-  	throw new PendingException();
+  	     throw new PendingException();
 	}
 
 	@And("^I have previously had <dollars> in the bank$")
 	public void iHavePreviouslyHadDollarsInTheBank(String dollars)  {
-  	throw new PendingException();
+  	     throw new PendingException();
 	}
 
 	@When("^I press the coffee button$")
 	public void iPressTheCoffeeButton() {
-  	throw new PendingException();
+  	     throw new PendingException();
 	}
 
 	@Then("^I should be served a coffee$")
 	public void iShouldBeServedCoffee() {
-  	throw new PendingException();
+  	     throw new PendingException();
+	}
+	
+	@Given("^there are (\\d+) coffees left in the machine$")
+	public void there_are_coffees_left_in_the_machine(int arg1) throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+	     throw new PendingException();
+	}
+
+	@Given("^I have deposited (\\d+)\\$$")
+	public void i_have_deposited_$(int arg1) throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+	     throw new PendingException();
+	}
+
+	@Given("^I previously had (\\d+)\\$ in the bank$")
+	public void i_previously_had_$_in_the_bank(int arg1) throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+	     throw new PendingException();
 	}
 }
